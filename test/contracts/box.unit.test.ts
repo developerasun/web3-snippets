58e3b4ddbb6706659567c7beb6d71c320c49e721
developerasun <nellow1102@gmail.com>
2023-09-24 21:27:52 +0900

    add L2 gas estimate test with equation

--- test/contracts/box.unit.test.ts
+++ test/contracts/box.unit.test.ts
@@ -1,12 +1,12 @@
 import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { expect } from "chai";
 import { ethers } from "hardhat";
-import { useDeployer, useOptismFetcher } from "@scripts/hook";
+import { useABIParser, useDeployer, useOptismFetcher } from "@scripts/hook";
 import { ContractTransactionReceipt, Filter, TransactionRequest } from "ethers";
 import hre from "hardhat";
 import { Box } from "@assets/types";
 
-const { ALCHEMY_KEY_MUMBAI } = process.env;
+const { ALCHEMY_KEY_MUMBAI, ALCHMEY_OPTIMISM_API_KEY } = process.env;
 
 const contractName = "Box";
 const PREFIX = `unit-${contractName}`;
@@ -71,16 +71,25 @@ describe(`${PREFIX}-gas`, function TestGas() {
     await useOptismFetcher();
   });
 
-  it.only("Should init a raw transaction", async function TestRawTransaction() {
+  it.skip("Should init a raw transaction", async function TestRawTransaction() {
     const { contract, owner } = await loadFixture(useFixture);
 
     const box = contract as unknown as Box;
     const param = 10;
+    const _maxFeePerGas = ethers.parseUnits("1", "gwei");
+    const _maxPriorityFeePerGas = ethers.parseUnits("3", "gwei");
+
+    console.log({ _maxFeePerGas });
+
+    console.log(BigInt(_maxFeePerGas));
+
     const txMeta: TransactionRequest = {
       from: owner.address,
 
       //The maximum total fee to pay per gas. The actual value used is protocol enforced to be the block's base fee.
-      maxFeePerGas: ethers.formatUnits("3", "gwei"),
+      // unit: gwei => bigint
+      maxFeePerGas: BigInt(_maxFeePerGas),
+      maxPriorityFeePerGas: BigInt(_maxPriorityFeePerGas),
     };
 
     // @dev `populateTransaction`: validate all props for tx
@@ -94,6 +103,55 @@ describe(`${PREFIX}-gas`, function TestGas() {
       console.log("tx status: ", receipt.status === 1 ? "success" : "failure");
     }
   });
+
+  it.only("Should estimate gas for L2 tx", async function TestL2Gas() {
+    const { contract } = await loadFixture(useFixture);
+    const [owner] = await ethers.getSigners();
+
+    const sdk = await import("@eth-optimism/fee-estimation");
+    const { getL2Client, estimateFees, overhead, scalar } = sdk;
+
+    const params = {
+      chainId: 420,
+      rpcUrl: "https://goerli.optimism.io",
+      // chainId: 10,
+      // rpcUrl: "https://mainnet.optimism.io",
+    };
+
+    const client = getL2Client(params);
+
+    const abi = (await useABIParser(contractName)).abi;
+
+    const gas = await estimateFees({
+      functionName: "setValue",
+      abi: abi,
+      args: [100],
+      chainId: 420,
+      client: client,
+      account: "0x7D5d9edDC3DAd91e5E18200EC3972CeCF988485f", // eoa address
+      to: "0xc2be2fE8A4B52348880E9D08277eF305A5Aad1F8", // contract address
+    });
+
+    const cost = ethers.formatUnits(gas, "ether");
+    console.log({ cost }); // cold : 142.75 krw, warm: 79.20 krw / 0.000037201501267602 ether
+
+    // from block explorer: https://goerli-optimism.etherscan.io/tx/0x9327d517d3dfd7175a85093e966c59f4fd812140d1ce9613483e7778e1b286b3
+    // actual testnet cost: cold : 146.92 krw(0.11 usd), warm 88.14 krw / 0.000041401501408358 ether
+
+    console.log({ gas });
+  });
 });
 
-describe(`${PREFIX}-assembly`, function TestAssembly() {});
+describe(`${PREFIX}-assembly`, function TestAssembly() {
+  it.skip("Should return a storage slot", async function TestStorageSlot() {
+    const { contract } = await loadFixture(useFixture);
+
+    const expected = 10;
+    expect(await contract.getDirectValue()).to.equal(expected);
+  });
+
+  it.skip("Should add two values", async function TestAdd() {
+    const { contract } = await loadFixture(useFixture);
+    expect(await contract.addTwoValues(3, 99)).to.equal(102);
+  });
+});

de3211aa6ced50d99a1078044d3c12c38902b73d
developerasun <nellow1102@gmail.com>
2023-09-30 01:58:10 +0900

    add prefix and format for eip191 and 712

--- scripts/constants.ts
+++ scripts/constants.ts
@@ -6,6 +6,29 @@ export const INTERFACE_ID = {
   erc1155: "0xd9b67a26",
 } as const;
 
+const HASH_HELPER = {
+  erc9999: {
+    domain: {
+      name: "Jake Sung", 
+      version: 1,
+      hex: "4A616B652053756E67", 
+      decimals: "1372078877088207433319"
+    }
+  },
+  eip191: {
+    format: {
+      bytes32: "\x19Ethereum Signed Message:\n32",
+      arbitrary: "\x19Ethereum Signed Message:\n",
+      validator: "\x19\x00Ethereum Signed Message:\n"
+    },
+    prefix: "\x19"
+  },
+  eip712: {
+    format: "\x19\x01\Ethereum Signed Message:\n32",
+    prefix: "\x19\x01"
+  },
+} as const
+
 export const HARDHAT_CONFIG_HELPER = {
   compiler: {
     enable: true,

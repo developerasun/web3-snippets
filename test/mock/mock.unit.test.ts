f288aa70e61c55282c0bd0bd4c4b92d8b0f3937e
developerasun <nellow1102@gmail.com>
2023-10-05 23:05:53 +0900

    add axios mock test case

--- test/mock/mock.unit.test.ts
+++ test/mock/mock.unit.test.ts
@@ -1,10 +1,26 @@
 import {describe, expect, test, jest} from '@jest/globals';
+import axios from 'axios'
+
+jest.mock('axios')
+
+const mAxios = axios as jest.Mocked<typeof axios>
 
 describe('Jest mock test', function TestJest() {
-    test.only("Should mock a function", function TestFunctionMock() {
+    test.skip("Should mock a function", function TestFunctionMock() {
         const mockFn = jest.fn()
         mockFn.mockReturnValue(3)
 
         expect(mockFn()).toEqual(3)
+    })
+
+    test.only("Should mock axios get", async function TestAxios() {
+        const shouldBe = { 
+            who: 'jake'
+        }
+        mAxios.get.mockResolvedValue(shouldBe)
+
+        const resp = await mAxios.get('https://jsonplaceholder.typicode.com/todos/1')
+        
+        expect(resp).toEqual(shouldBe)
     })
 });

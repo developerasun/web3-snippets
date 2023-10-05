5e7d972adfbf03c98eee495fdb75402f302664a4
developerasun <nellow1102@gmail.com>
2023-10-05 23:32:31 +0900

    add api failure test cases

--- test/mock/mock.unit.test.ts
+++ test/mock/mock.unit.test.ts
@@ -1,4 +1,4 @@
-import {describe, expect, test, jest} from '@jest/globals';
+import {describe, expect, test, jest, afterEach} from '@jest/globals';
 import axios from 'axios'
 import { mocked } from 'jest-mock'
 
@@ -8,6 +8,11 @@ const _mAxios = mocked(axios)
 const mAxios = axios as jest.Mocked<typeof axios>
 
 describe('Jest mock test', function TestJest() {
+
+    afterEach(() => {
+        _mAxios.mockReset()
+    })
+
     test.skip("Should mock a function", function TestFunctionMock() {
         const mockFn = jest.fn()
         mockFn.mockReturnValue(3)
@@ -15,7 +20,7 @@ describe('Jest mock test', function TestJest() {
         expect(mockFn()).toEqual(3)
     })
 
-    test.only("Should mock axios get", async function TestAxios() {
+    test.skip("Should mock axios get", async function TestAxios() {
         const shouldBe = { 
             who: 'jake'
         }
@@ -26,13 +31,25 @@ describe('Jest mock test', function TestJest() {
         expect(resp).toEqual(shouldBe)
     })
 
-    test.only("Should mock without typecasting", async function TestNoTypeMock() {
+    test.skip("Should mock without typecasting", async function TestNoTypeMock() {
         const shouldBe = {
             who: 'jake'
         }
 
         _mAxios.get.mockResolvedValue(shouldBe)
+        
         const resolved = await _mAxios.get("https://jsonplaceholder.typicode.com/todos/1")
+        
         expect(resolved).toEqual(shouldBe)
+
+        expect(_mAxios.get).toHaveBeenCalledWith("https://jsonplaceholder.typicode.com/todos/1")
+    })
+    
+    test.only("Should throw error", async function TestApiFailure() {
+        _mAxios.get.mockRejectedValueOnce(new Error("api failed"))
+        await expect(_mAxios.get("https://jsonplaceholder.typicode.com/todos/1")).rejects.toThrow("api failed")
+
+        // promise called but not resolved
+        expect(_mAxios.get).toHaveBeenCalledWith("https://jsonplaceholder.typicode.com/todos/1")
     })
 });

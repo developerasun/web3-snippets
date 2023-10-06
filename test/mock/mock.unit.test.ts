5908ffa1eec68cc6ae01354e1765199a2dff71c5
developerasun <nellow1102@gmail.com>
2023-10-06 23:17:31 +0900

    add jest timer test case

--- test/mock/mock.unit.test.ts
+++ test/mock/mock.unit.test.ts
@@ -2,7 +2,7 @@ import {describe, expect, test, jest, afterEach} from '@jest/globals';
 import axios from 'axios'
 import { mocked } from 'jest-mock'
 
-
+jest.useFakeTimers()
 jest.mock('axios')
 const _mAxios = mocked(axios)
 const mAxios = axios as jest.Mocked<typeof axios>
@@ -10,7 +10,7 @@ const mAxios = axios as jest.Mocked<typeof axios>
 describe('Jest mock test', function TestJest() {
 
     afterEach(() => {
-        _mAxios.mockReset()
+        jest.clearAllMocks()
     })
 
     test.skip("Should mock a function", function TestFunctionMock() {
@@ -45,11 +45,31 @@ describe('Jest mock test', function TestJest() {
         expect(_mAxios.get).toHaveBeenCalledWith("https://jsonplaceholder.typicode.com/todos/1")
     })
     
-    test.only("Should throw error", async function TestApiFailure() {
+    test.skip("Should throw error", async function TestApiFailure() {
         _mAxios.get.mockRejectedValueOnce(new Error("api failed"))
         await expect(_mAxios.get("https://jsonplaceholder.typicode.com/todos/1")).rejects.toThrow("api failed")
 
         // promise called but not resolved
         expect(_mAxios.get).toHaveBeenCalledWith("https://jsonplaceholder.typicode.com/todos/1")
+    })
+
+    test.only("Should use fake timer", async function TestJestTimer() {
+        const logInLog = (cb: () => void) => {
+            console.log("log right away")
+
+            setTimeout(() => {
+                cb && cb()
+            }, 2000);
+        }
+
+        const callback = jest.fn(() => console.log("log in two seconds"))
+
+        logInLog(callback)
+
+        expect(callback).not.toBeCalled()
+
+        jest.advanceTimersByTime(2000)
+
+        expect(callback).toBeCalledTimes(1)
     })
 });

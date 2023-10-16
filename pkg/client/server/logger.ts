12a870039513c0b9870a4c0799e08204d91a1b1c
developerasun <nellow1102@gmail.com>
2023-10-16 22:46:33 +0900

    configure logger and write to file

--- pkg/client/server/logger.ts
+++ pkg/client/server/logger.ts
@@ -1,10 +1,33 @@
 import pino from 'pino'
+import { mkdirSync, mkdir } from 'fs'
+import { promisify } from 'util'
 
-const logPath = 'server/log/log.txt'
+const _mkdir = promisify(mkdir)
+
+const baseDir = 'server/log'
+
+const _initLogDir = async () => {
+    await _mkdir(baseDir, { recursive: true })
+}
+
+const initLogDir = () => {
+    mkdirSync(baseDir, {recursive: true})
+}
+
+const getDailyLogFile = () => {
+    initLogDir()
+
+    const date = new Date()
+    const format = `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}.log`
+    return format
+}
+
+const fullLogPath = `${baseDir}/${getDailyLogFile()}`
+
 export const logger = pino({
     timestamp: pino.stdTimeFunctions.isoTime,
     base: undefined
 }, pino.destination({
-    dest:  logPath,
+    dest:  fullLogPath,
     append: true
 }))

eef9f67ad18ff30dd3a0ba2b7a1f0c855265817b
developerasun <nellow1102@gmail.com>
2023-10-16 21:20:01 +0900

    init pnpm workspaces

--- .solcover.cjs
+++
@@ -1,4 +0,0 @@
-// solidity-coverage plugin config
-module.exports = {
-  istanbulFolder: "./test/coverage",
-};
--- arguments.ts
+++
@@ -1,7 +0,0 @@
-const exampleArguments = ["constructor argument 1", "constructor argument 2", "constructor argument 3"];
-
-const upgradeableArguments = [
-  // upgradeable does not require constructor
-];
-
-export { exampleArguments };

1e3173a817266f76dc0ca1243e19790ce375e170
developerasun <nellow1102@gmail.com>
2023-10-17 23:28:29 +0900

    configure apidoc and serve for api doc gen

---
+++ pkg/doc/src/apidoc.js
@@ -0,0 +1,12 @@
+
+/**
+ * @api {get} /api/test get test response
+ *
+ * @apiVersion 1.0.0
+ * @apiName getTest
+ * @apiGroup test
+ *
+
+ * @apiSuccess {Object} get greeting message `hello jake` as json object.
+|
+ */

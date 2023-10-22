522b2e73d6a7d3e752531b81efc6cb7fffb7800b
developerasun <nellow1102@gmail.com>
2023-10-22 23:54:37 +0900

    add test cases for generic, overload, polymorphism

--- pkg/blockchain/test/contracts/hooks.unit.test.ts
+++ pkg/blockchain/test/contracts/hooks.unit.test.ts
@@ -63,10 +63,58 @@ describe(`${PREFIX}-utility-hooks`, function TestHookFunctions() {
         console.log("next promise execution index at: ", retryAt)
     })
 
-    it.only("Should log epoch time in seconds", function TestEpochTime() {
+    it.skip("Should log epoch time in seconds", function TestEpochTime() {
         const {utc, local} = useEpochTime()
 
         console.log({utc})
         console.log({local})
+    })
+})
+
+describe(`${PREFIX}-OOP`, function TestObjectOriented() {
+    it.only("Should return different values with generics", function TestGeneric() {
+        function returnValue<T>(name: T): T {
+            return name
+        }
+
+        expect(returnValue<number>(3)).to.equal(3)
+
+        const target = { name: 'Jake' }
+        expect(returnValue(target)).to.equal(target)
+    })
+
+    it.only("Should make different function signature", function TestOverloading() {
+        // overload signature
+        function greetings(name: string): string;
+        function greetings(age: number): number
+
+        // overload implementation
+        function greetings(arg: string | number) {
+            return arg
+        }
+
+        expect(greetings('jake')).to.equal('jake')
+        expect(greetings(29)).to.equal(29)
+    })
+
+    it.only("Should implement an interface", function TestImplementation() {
+        interface greetings {
+            sayHi: () => string
+        }
+
+        class Hello implements greetings {
+            private _name: string
+
+            constructor(name: string) {
+                this._name = name
+            }
+
+            sayHi() {
+                return this._name
+            }
+        }
+
+        const hello = new Hello('jake')
+        expect( hello.sayHi()).to.equal('jake')
     })
 })

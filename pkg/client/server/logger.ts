aede3f9dd80929e4972da4c6502fe50c8394e2fa
developerasun <nellow1102@gmail.com>
2023-10-29 21:29:00 +0900

    add overload and decorator example

--- pkg/client/server/logger.ts
+++ pkg/client/server/logger.ts
@@ -1,39 +1,97 @@
-import pino from 'pino'
-import { mkdirSync, mkdir } from 'fs'
-import { promisify } from 'util'
+import pino from "pino";
+import { mkdirSync, mkdir } from "fs";
+import { promisify } from "util";
 
-const _mkdir = promisify(mkdir)
+const _mkdir = promisify(mkdir);
 
-const baseDir = 'server/log'
+const baseDir = "server/log";
 
 const _initLogDir = async () => {
-    await _mkdir(baseDir, { recursive: true })
-}
+  await _mkdir(baseDir, { recursive: true });
+};
 
 const initLogDir = () => {
-    mkdirSync(baseDir, {recursive: true})
-}
+  mkdirSync(baseDir, { recursive: true });
+};
 
 const getDailyLogFile = () => {
-    initLogDir()
-    
-    const date = new Date()
-    const format = `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}.log`
-    return format
-}
+  initLogDir();
 
-const fullLogPath = `${baseDir}/${getDailyLogFile()}`
+  const date = new Date();
+  const format = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}.log`;
+  return format;
+};
+
+const fullLogPath = `${baseDir}/${getDailyLogFile()}`;
 
 const streams = [
-    { stream : process.stdout }, 
-    { stream : pino.destination({
-        dest: fullLogPath, 
-        append: true,
-        sync: true
-    })}, 
-]
+  { stream: process.stdout },
+  {
+    stream: pino.destination({
+      dest: fullLogPath,
+      append: true,
+      sync: true,
+    }),
+  },
+];
 
-export const logger = pino({
+export const logger = pino(
+  {
     timestamp: pino.stdTimeFunctions.isoTime,
-    base: undefined
-}, pino.multistream(streams))
+    base: undefined,
+  },
+  pino.multistream(streams)
+);
+
+@_logContext
+export class LogContext {
+  platform: string[];
+
+  constructor(...args: string[]) {
+    this.platform = args;
+  }
+
+  // function signature
+  overload(): void;
+  overload(message: number): void;
+
+  // method implementation
+  overload(message?: number) {
+    if (message) {
+      console.log("second signature: ", message);
+    } else {
+      console.log("first signature: void param");
+    }
+  }
+
+  @logSender("jake") // method decorator
+  doLog() {
+    logger.info(`this will be logged in second order`);
+  }
+
+  getParams() {
+    return this.platform;
+  }
+}
+
+function logSender(message: string) {
+  console.log("executed in closure");
+  console.log(`got message from: ${message}`);
+
+  return function (target: any, key: string, desc: PropertyDescriptor) {
+    const method = desc.value;
+
+    console.log({ target });
+    console.log({ key });
+    desc.value = function () {
+      console.log("changing original method behavior");
+      method(message);
+      console.log("decorator done");
+    };
+  };
+}
+
+function _logContext(constructor: typeof LogContext) {
+  console.log(constructor);
+  console.log(constructor.name);
+}

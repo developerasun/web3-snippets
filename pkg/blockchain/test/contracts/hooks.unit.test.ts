71e688625e92d611ca263c4a20b09e349b98cb53
developerasun <nellow1102@gmail.com>
2023-10-31 00:02:16 +0900

    add promise error handling cases

--- pkg/blockchain/test/contracts/hooks.unit.test.ts
+++ pkg/blockchain/test/contracts/hooks.unit.test.ts
@@ -1,120 +1,200 @@
-import { logger, useCron, useEpochTime, useInterval } from '@scripts/hook';
-import { expect } from 'chai';
+import { logger, useCron, useEpochTime, useInterval } from "@scripts/hook";
+import { expect } from "chai";
 
-const PREFIX="hooks"
+const PREFIX = "hooks";
 
 describe(`${PREFIX}-utility-hooks`, function TestHookFunctions() {
-    it.skip("Should execute cron jobs every 1 mins", async function TestCron() {
-        const callback = () => console.log("check")
-        const everyOneMinute = "*/1 * * * *" 
-        useCron(everyOneMinute, callback)
-    })   
-  
-    it.skip("Should execute cron jobs by 2 secs", async function TestInterval() {
-        const handler = () => console.log("now: ", Date.now())
-        const params = {
-            timeout: 2000, 
-            clear: 6000 
-        } 
+  it.skip("Should execute cron jobs every 1 mins", async function TestCron() {
+    const callback = () => console.log("check");
+    const everyOneMinute = "*/1 * * * *";
+    useCron(everyOneMinute, callback);
+  });
 
-        const {done} = await useInterval(handler, params.timeout, params.clear)
-        
-        expect(done).to.be.true
-    }) 
+  it.skip("Should execute cron jobs by 2 secs", async function TestInterval() {
+    const handler = () => console.log("now: ", Date.now());
+    const params = {
+      timeout: 2000,
+      clear: 6000,
+    };
 
-    it.skip("should log in two seconds", function TestLogInTwoSeconds() {
-        setTimeout(() => {
-            console.log("Wefwef")
-        }, 2000);
-    })
+    const { done } = await useInterval(handler, params.timeout, params.clear);
 
-    it.skip("Should check promise orders", async function TestPromiseOrder() {
-        let success: number[] = []
+    expect(done).to.be.true;
+  });
 
-        let retryAt: number = 999
+  it.skip("should log in two seconds", function TestLogInTwoSeconds() {
+    setTimeout(() => {
+      console.log("Wefwef");
+    }, 2000);
+  });
 
-        async function pLog(index: number) {
-            return await new Promise((resolve, reject) => {
-                console.log(index)
+  it.skip("Should check promise orders", async function TestPromiseOrder() {
+    let success: number[] = [];
 
-                if (index !== 8) { // escape at index 8
-                    success.push(index)
-                    resolve(true)
-                } else {
-                    reject(false)
-                }
-            })
+    let retryAt: number = 999;
+
+    async function pLog(index: number) {
+      return await new Promise((resolve, reject) => {
+        console.log(index);
+
+        if (index !== 8) {
+          // escape at index 8
+          success.push(index);
+          resolve(true);
+        } else {
+          reject(false);
         }
- 
-        try {
-            for (let index = 0; index < 10; index++) {
-                await pLog(index) // sequential execution 
-            }  
-        } catch (error) {   
-            console.error(error)
-        } finally {   
-            console.log("done")
-            console.log(success)
-            console.log("success till index: ", success.length-1)
-            console.log("Failed at index: ", success.length)
-            retryAt = success.length
-        } 
+      });
+    }
 
-        console.log("next promise execution index at: ", retryAt)
-    })
+    try {
+      for (let index = 0; index < 10; index++) {
+        await pLog(index); // sequential execution
+      }
+    } catch (error) {
+      console.error(error);
+    } finally {
+      console.log("done");
+      console.log(success);
+      console.log("success till index: ", success.length - 1);
+      console.log("Failed at index: ", success.length);
+      retryAt = success.length;
+    }
 
-    it.skip("Should log epoch time in seconds", function TestEpochTime() {
-        const {utc, local} = useEpochTime()
+    console.log("next promise execution index at: ", retryAt);
+  });
 
-        console.log({utc})
-        console.log({local})
-    })
-})
+  it.skip("Should log epoch time in seconds", function TestEpochTime() {
+    const { utc, local } = useEpochTime();
+
+    console.log({ utc });
+    console.log({ local });
+  });
+});
 
 describe(`${PREFIX}-OOP`, function TestObjectOriented() {
-    it.only("Should return different values with generics", function TestGeneric() {
-        function returnValue<T>(name: T): T {
-            return name
-        }
+  it.skip("Should return different values with generics", function TestGeneric() {
+    function returnValue<T>(name: T): T {
+      return name;
+    }
 
-        expect(returnValue<number>(3)).to.equal(3)
+    expect(returnValue<number>(3)).to.equal(3);
 
-        const target = { name: 'Jake' }
-        expect(returnValue(target)).to.equal(target)
-    })
+    const target = { name: "Jake" };
+    expect(returnValue(target)).to.equal(target);
+  });
 
-    it.only("Should make different function signature", function TestOverloading() {
-        // overload signature
-        function greetings(name: string): string;
-        function greetings(age: number): number
+  it.skip("Should make different function signature", function TestOverloading() {
+    // overload signature
+    function greetings(name: string): string;
+    function greetings(age: number): number;
 
-        // overload implementation
-        function greetings(arg: string | number) {
-            return arg
-        }
+    // overload implementation
+    function greetings(arg: string | number) {
+      return arg;
+    }
 
-        expect(greetings('jake')).to.equal('jake')
-        expect(greetings(29)).to.equal(29)
-    })
+    expect(greetings("jake")).to.equal("jake");
+    expect(greetings(29)).to.equal(29);
+  });
 
-    it.only("Should implement an interface", function TestImplementation() {
-        interface greetings {
-            sayHi: () => string
-        }
+  it.skip("Should implement an interface", function TestImplementation() {
+    interface greetings {
+      sayHi: () => string;
+    }
 
-        class Hello implements greetings {
-            private _name: string
+    class Hello implements greetings {
+      private _name: string;
 
-            constructor(name: string) {
-                this._name = name
-            }
+      constructor(name: string) {
+        this._name = name;
+      }
 
-            sayHi() {
-                return this._name
-            }
-        }
+      sayHi() {
+        return this._name;
+      }
+    }
 
-        const hello = new Hello('jake')
-        expect( hello.sayHi()).to.equal('jake')
-    })
-})
+    const hello = new Hello("jake");
+    expect(hello.sayHi()).to.equal("jake");
+  });
+});
+
+describe(`${PREFIX}-promise-handle`, function TestHandlePromise() {
+  const handle = async (promise: Promise<unknown>) => {
+    // prettier-ignore
+    return promise
+                .then((data) => { return [data, null]})
+                .catch((error) => { return [null, error]})
+  };
+
+  async function typedHandle<T>(promise: Promise<T>) {
+    // prettier-ignore
+    return promise
+          .then((data: T) => {
+              return { data, error: undefined }
+          })
+          .catch((error: unknown) => { return { data: undefined, error } }) // error is not guaranteed to be an Error object. unknown type is recommended and mandatory
+  }
+
+  it.skip("Should resolve with handler", async function TestResolve() {
+    const resolved = new Promise((resolve) => resolve({ name: "Jake" }));
+    const [data, error] = await handle(resolved);
+
+    if (error) {
+      console.error(error);
+    }
+
+    if (data) {
+      console.log({ data });
+    }
+
+    expect(error).to.be.null;
+    expect(data).not.to.be.null;
+  });
+
+  it.skip("Should reject with handler", async function TestReject() {
+    const rejected = new Promise((_, reject) => reject("REJECT: no reason"));
+    const [data, error] = await handle(rejected);
+
+    if (error) {
+      console.error(error);
+    }
+
+    if (data) {
+      console.log({ data });
+    }
+  });
+
+  it.skip("Should resolve with return type", async function TestInferReturnType() {
+    interface IHuman {
+      name: string;
+    }
+
+    const resolved = new Promise<IHuman>((resolve, _) => resolve({ name: "Jake" }));
+
+    const rHuman = await typedHandle<IHuman>(resolved);
+
+    if (rHuman.data) {
+      console.log(rHuman.data.name);
+      expect(rHuman.data.name).to.equal("Jake");
+    }
+
+    expect(rHuman.error).to.be.undefined;
+  });
+
+  it.only("Should reject but not throw", async function TestNoException() {
+    interface ICat {
+      sound: string
+    }
+
+    const rejected = new Promise<ICat>((_, reject) => reject("REJECT: woof"))
+    const rCat = await typedHandle<ICat>(rejected)
+
+    if (rCat.error) { 
+      console.log("got rejected message : ", rCat.error)
+    }
+
+    expect(rCat.error).to.equal("REJECT: woof")
+  })
+});

309416e7fd67c493acb6061a2b97015a50390ecf
developerasun <nellow1102@gmail.com>
2023-11-09 23:59:03 +0900

    check headers from the middleware

--- pkg/client/app/api/test/route.ts
+++ pkg/client/app/api/test/route.ts
@@ -1,15 +1,21 @@
-import { LogContext, logger } from "@app/server/logger";
+import { LogContext, logger } from "@/server/logger";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
-  const ctx = new LogContext("windows", "mac", "linux");
-  const params = ctx.getParams();
-  console.log("constructor args: ", params);
+  // const ctx = new LogContext("windows", "mac", "linux");
+  // const params = ctx.getParams();
+  // console.log("constructor args: ", params);
 
-  // method overloading
-  ctx.doLog();
-  ctx.overload();
-  ctx.overload(456789);
+  // // method overloading
+  // ctx.doLog();
+  // ctx.overload();
+  // ctx.overload(456789);
+  console.log("test api");
+  const fromMiddleware = request.headers.get("magic-value");
+  const _globalFoo = request.headers.get("global-foo");
+
+  console.log({ fromMiddleware });
+  console.log({ _globalFoo });
 
   return NextResponse.json({
     message: "hello jake",

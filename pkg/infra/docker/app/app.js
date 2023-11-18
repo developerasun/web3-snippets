09a5107383a240bdb0d2db1b2744d585248350ab
Jake Sung <asuntheawesome@gmail.com>
2023-11-19 01:14:14 +0900

    check docker secret value from file, gpg-encrypted env value

--- pkg/infra/docker/app/app.js
+++ pkg/infra/docker/app/app.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
 
-app.get("/", function (req, res) {
+app.get("/", async function (req, res) {
   res.json({
     message: "check env with no env file",
     value: `FOO: ${process.env.FOO}`,
-    secret: process.env.SUPER_SECRET,
+    secret: process.env.SUPER_SECRET ?? "not detected",
+    check: process.env.MESSAGE ?? "no value",
   });
 });
 

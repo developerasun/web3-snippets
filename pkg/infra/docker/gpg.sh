a9c99403df156dcb515ec38f59049099ea75e38b
Jake Sung <asuntheawesome@gmail.com>
2023-11-19 01:11:57 +0900

    add gpg snippets for encrypt and skip passphrase

--- pkg/infra/docker/gpg.sh
+++ pkg/infra/docker/gpg.sh
@@ -1,2 +1,11 @@
 gpg -a --export --output ./pub.asc nellow1102@gmail.com
 gpg -a --export-secret-keys > ./pk.asc
+
+exportAsHexMessage() {
+    gpg --encrypt --recipient <recipient_name> --armor <file_name>
+}
+
+noPasspharse() {
+    gpg --edit-keys <key_name>
+    passwd # blank and enter
+}

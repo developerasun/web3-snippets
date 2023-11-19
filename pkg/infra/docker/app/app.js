aaa151f8a11fbfef84603700b8dbf413536d8d5e
Jake Sung <asuntheawesome@gmail.com>
2023-11-19 15:19:14 +0900

    add container running check

--- pkg/infra/docker/app/app.js
+++ pkg/infra/docker/app/app.js
@@ -1,12 +1,16 @@
 const express = require("express");
+
 const app = express();
 
 app.get("/", async function (req, res) {
+  const isContainer = (await import("is-docker")).default();
+
   res.json({
     message: "check env with no env file",
     value: `FOO: ${process.env.FOO}`,
     secret: process.env.SUPER_SECRET ?? "not detected",
-    check: process.env.MESSAGE ?? "no value",
+    encryptedMessage: process.env.MESSAGE ?? "no value",
+    isDocker: isContainer,
   });
 });
 

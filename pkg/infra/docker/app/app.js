f1f357e9dc91340732d9c53685239ddc067a371b
Jake Sung <asuntheawesome@gmail.com>
2023-11-19 17:34:17 +0900

    check container runs

--- pkg/infra/docker/app/app.js
+++ pkg/infra/docker/app/app.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const app = express();
 
@@ -6,11 +8,10 @@ app.get("/", async function (req, res) {
   const isContainer = (await import("is-docker")).default();
 
   res.json({
-    message: "check env with no env file",
-    value: `FOO: ${process.env.FOO}`,
-    secret: process.env.SUPER_SECRET ?? "not detected",
-    encryptedMessage: process.env.MESSAGE ?? "no value",
     isDocker: isContainer,
+    value: `FOO: ${process.env.FOO}`,
+    fromSecret: process.env.SUPER_SECRET ?? "not in a swarm",
+    fromCompose: process.env.MESSAGE ?? "no value",
   });
 });
 

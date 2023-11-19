c5e0bb53a233e190b84cc1fbcf67ec57d09010bc
Jake Sung <asuntheawesome@gmail.com>
2023-11-19 15:27:25 +0900

    split secret case by dotenv and constant

--- pkg/infra/docker/app/encrypt.js
+++ pkg/infra/docker/app/encrypt.js
@@ -1,6 +1,9 @@
 const openpgp = require("openpgp");
+const dotenv = require("dotenv");
+dotenv.config()
 
-const secret = "my cool message";
+const envSecret = process.env.ENV_SECRET ?? "not detected";
+const superSecret = "all is well" ?? "not detected";
 
 (async () => {
   const { privateKey, publicKey } = await openpgp.generateKey({ curve: "curve25519", userIDs: [{ name: "jake", email: "nellow1102@gmail.com" }] }); // returns armored value
@@ -8,7 +11,7 @@ const secret = "my cool message";
   const readPubKey = await openpgp.readKey({ armoredKey: publicKey });
   const readPkKey = await openpgp.readPrivateKey({ armoredKey: privateKey });
 
-  const message = await openpgp.createMessage({ text: secret, format: "text" });
+  const message = await openpgp.createMessage({ text: envSecret, format: "text" });
 
   const encrypted = await openpgp.encrypt({
     message,
@@ -19,10 +22,35 @@ const secret = "my cool message";
     armoredMessage: encrypted,
   });
 
-  const { data, signatures } = await openpgp.decrypt({
+  const { data } = await openpgp.decrypt({
     message: target,
     decryptionKeys: readPkKey,
   });
 
-  console.log({ data, signatures });
+  console.log({ data });
+})();
+
+(async () => {
+  const { privateKey, publicKey } = await openpgp.generateKey({ curve: "curve25519", userIDs: [{ name: "jake", email: "nellow1102@gmail.com" }] }); // returns armored value
+
+  const readPubKey = await openpgp.readKey({ armoredKey: publicKey });
+  const readPkKey = await openpgp.readPrivateKey({ armoredKey: privateKey });
+
+  const message = await openpgp.createMessage({ text: superSecret, format: "text" });
+
+  const encrypted = await openpgp.encrypt({
+    message,
+    encryptionKeys: readPubKey,
+  });
+
+  const target = await openpgp.readMessage({
+    armoredMessage: encrypted,
+  });
+
+  const { data } = await openpgp.decrypt({
+    message: target,
+    decryptionKeys: readPkKey,
+  });
+
+  console.log({ data });
 })();
